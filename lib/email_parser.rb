0471e8c5d8211faa9d94f458abf2e1ccdf68f2f8
lrh2daizzo <herrera.lrh@gmail.com>
2023-03-12 17:13:31 -0500

    doneskii

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -6,5 +6,13 @@ require 'pry'
 # or whitespace (' ').
 
 class EmailAddressParser
- 
+    attr_reader :emails
+
+    def initialize(emails)
+        @emails = emails
+    end
+
+    def parse 
+        @emails.split(/, | /).uniq
+    end
 end
